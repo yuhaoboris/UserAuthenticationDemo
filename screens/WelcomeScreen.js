2fa4ac9a2d82cb010edfa137b16fd49bf394fde8
boris <yuhaoboris@gmail.com>
2024-01-13 15:24:48 +0800

    add: fetch data with authentication roles but got HTTP 401

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,27 @@
+import axios from 'axios'
+import { useEffect, useState, useContext } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { AuthContext } from '../store/auth-context'
 
 function WelcomeScreen() {
+  const [message, setMessage] = useState('')
+
+  const authCtx = useContext(AuthContext)
+  const token = authCtx.token
+
+  useEffect(() => {
+    axios
+      .get('https://expense-tracker-70e33-default-rtdb.asia-southeast1.firebasedatabase.app/message.json?auth=' + token)
+      .then((res) => {
+        setMessage(res.data)
+      })
+  }, [token])
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{message}</Text>
     </View>
   )
 }
