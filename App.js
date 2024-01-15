76e95ccfbbf8fcb102f379e722f1c1c37ec31ead
boris <yuhaoboris@gmail.com>
2024-01-15 11:00:47 +0800

    add: store token to local storage

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { StatusBar } from 'expo-status-bar'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import AppLoading from 'expo-app-loading'
 
 import LoginScreen from './screens/LoginScreen'
 import SignupScreen from './screens/SignupScreen'
@@ -61,13 +63,38 @@ function Navigation() {
   )
 }
 
+function Root() {
+  const [isTryingLogin, setIsTryingLogin] = useState(true)
+  const authCtx = useContext(AuthContext)
+
+  useEffect(() => {
+    async function fetchToken() {
+      const storedToken = await AsyncStorage.getItem('token')
+
+      if (storedToken) {
+        authCtx.authenticate(storedToken)
+      }
+
+      setIsTryingLogin(false)
+    }
+
+    fetchToken()
+  }, [])
+
+  if (isTryingLogin) {
+    return <AppLoading />
+  }
+
+  return <Navigation />
+}
+
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
 
       <AuthContextProvider>
-        <Navigation />
+        <Root />
       </AuthContextProvider>
     </>
   )
